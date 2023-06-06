2736eb4466fc12a1feb4ecd759eaf502bb41e144
Josue Pujols (ClaroDom) <josue_pujols@claro.com.do>
2023-06-06 17:05:28 -0400

    removed program changes

--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Anagram;
-
 Console.WriteLine("Hello, World!");
-
-foreach(var item in await FileReader.ReadFile())
-    Console.WriteLine(item);
