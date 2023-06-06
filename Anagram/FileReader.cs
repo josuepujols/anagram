c106d7bcea4a800a62933ae1573d15d496ca2a2d
Josue Pujols (ClaroDom) <josue_pujols@claro.com.do>
2023-06-06 17:05:04 -0400

    added methos to list

---
+++ Anagram/FileReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class FileReader
+    {
+        public async static Task<string[]> ReadFile() => await File.ReadAllLinesAsync(@"C:\Users\Josue Pujols\OneDrive\Escritorio\Practica SCRUM\Practica 2\words.txt");
+    }
+}
--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Anagram;
+
 Console.WriteLine("Hello, World!");
+
+foreach(var item in await FileReader.ReadFile())
+    Console.WriteLine(item);
