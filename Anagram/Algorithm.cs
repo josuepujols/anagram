938bd03ba80fe2232df6aac94ca7c252ae567f4e
Rafael Cruz <jose_rafael_cruz@claro.com.do>
2023-06-13 13:50:27 -0400

    Refactor: on FindAnagrams logic

--- Anagram/Algorithm.cs
+++ Anagram/Algorithm.cs
@@ -4,13 +4,12 @@
     {
         public static IEnumerable<string[]> FindAnagrams(IEnumerable<string> candidates)
         {
-            if (null == candidates)
-                throw new ArgumentNullException(nameof(candidates));
+            if (null == candidates) throw new ArgumentNullException(nameof(candidates));
 
-            return candidates
-              .GroupBy(word => string.Concat(word.OrderBy(c => c)))
-              .Where(group => group.Count() > 1)
-              .Select(group => group.OrderBy(word => word).ToArray());
+            IEnumerable<IGrouping<string, string>> anagramGrouped = candidates.GroupBy(word => string.Concat(word.OrderBy(c => c)));
+            IEnumerable<IGrouping<string, string>> groupByAnagramCount = anagramGrouped.Where(group => group.Count() > 1);
+            IEnumerable<string[]> result = groupByAnagramCount.Select(group => group.OrderBy(word => word).ToArray());
+            return result;
         }
     }
 }
